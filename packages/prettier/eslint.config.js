e3f7036b26f849dfd483285f05335f9931a6f0a0
demonicattack <demonicattacks@outlook.com>
2024-10-10 00:41:22 +0400

    chore: update eslint config

--- packages/prettier/eslint.config.js
+++ packages/prettier/eslint.config.js
@@ -2,5 +2,8 @@ import { config } from '@demonicattack/eslint';
 
 export default config({
     type: 'lib',
-    ts: true,
+    ts: {
+      tsconfigPath: 'tsconfig.json',
+    },
+    "no-commented-code": true,
 });
